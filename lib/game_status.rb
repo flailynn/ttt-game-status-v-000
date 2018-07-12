c04f29b3579f0fa4d5988b2143c77a86d841ad63
Anita Beauchamp <amadamspr@gmail.com>
2018-07-12 03:38:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,6 +21,18 @@ def won?(board)
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1].upcase
+    position_2 = board[win_index_1].upcase
+    position_3 = board[win_index_1].upcase
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    else
+      false
+    end
+ 
   end
 end
-  
