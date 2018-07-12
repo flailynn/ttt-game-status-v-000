3a2bb588dbfd904cf25a9d99c4c39cd95efc4334
Anita Beauchamp <amadamspr@gmail.com>
2018-07-12 03:57:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS = [
 
 # #won? method determines if the game has been won by a player
 def won?(board)
-WIN_COMBINATIONS.select do |win_combination|
+WIN_COMBINATIONS.detect do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
@@ -38,5 +38,5 @@ WIN_COMBINATIONS.select do |win_combination|
 
   end
 
-  
+
 end
